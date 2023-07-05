66b7e8c2f16deea0caa2c0f0593cf9fb6a51a687
hyobin2384 <80449101+hyobin2384@users.noreply.github.com>
2023-07-05 13:04:38 +0900

    첫 커밋

---
+++ src/main/java/com/example/pinterest/PinterestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinterestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinterestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/pinterest/hello.java
@@ -0,0 +1,17 @@
+package com.example.pinterest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class hello {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello");
+        return "hello";
+    }
+
+}
